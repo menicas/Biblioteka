14a8385a709d691961a36516a0f4c80d72b20cad
Paweł <pawel.bednarz2@gmail.com>
2019-03-13 18:01:25 +0100

    little refactor

--- src/projektbiblioteka/ProjektBiblioteka.java
+++ src/projektbiblioteka/ProjektBiblioteka.java
@@ -8,23 +8,8 @@ public class ProjektBiblioteka {
         Seed s = new Seed(biblioteka);
         s.seed();
 
-        System.out.println(biblioteka.zwrocIleKsiazek() + "\n\n\n");
 
         Menu menu = new Menu(biblioteka);
-        menu.menu();
-
-    /*
-        Biblioteka biblioteka = new Biblioteka();
-        //biblioteka.dodajNowaKsiazke();
-        biblioteka.wyswietlListeKsiazek();
-
-        biblioteka.wypozyczKsiazke(0);
-        biblioteka.wypozyczKsiazke(1);
-        biblioteka.zwrocKsiazke(1);
-        biblioteka.wypozyczKsiazke(0);
-        biblioteka.zwrocKsiazke(1);
-        biblioteka.wyswietlSkroconaListeKsiazek();
-        biblioteka.wyswietlListeKsiazek();
-        */
+        menu.wyswietlMenu();
     }
 }
--- src/projektbiblioteka/Seed.java
+++ src/projektbiblioteka/Seed.java
@@ -11,39 +11,39 @@ public class Seed {
     public void seed() {
         biblioteka.wypozyczKsiazke(1);
         biblioteka.zwrocKsiazke(1);
-        biblioteka.wypozyczKsiazke(1);
-        biblioteka.zwrocKsiazke(1);
-        biblioteka.wypozyczKsiazke(1);
-        biblioteka.zwrocKsiazke(1);
-        biblioteka.wypozyczKsiazke(1);
-        biblioteka.zwrocKsiazke(1);
-        biblioteka.wypozyczKsiazke(1);
-        biblioteka.zwrocKsiazke(1);
+//        biblioteka.wypozyczKsiazke(1);
+//        biblioteka.zwrocKsiazke(1);
+//        biblioteka.wypozyczKsiazke(1);
+//        biblioteka.zwrocKsiazke(1);
+//        biblioteka.wypozyczKsiazke(1);
+//        biblioteka.zwrocKsiazke(1);
+//        biblioteka.wypozyczKsiazke(1);
+//        biblioteka.zwrocKsiazke(1);
 
         biblioteka.wypozyczKsiazke(2);
         biblioteka.zwrocKsiazke(2);
-        biblioteka.wypozyczKsiazke(2);
-        biblioteka.zwrocKsiazke(2);
-        biblioteka.wypozyczKsiazke(2);
-        biblioteka.zwrocKsiazke(2);
-        biblioteka.wypozyczKsiazke(2);
-        biblioteka.zwrocKsiazke(2);
+//        biblioteka.wypozyczKsiazke(2);
+//        biblioteka.zwrocKsiazke(2);
+//        biblioteka.wypozyczKsiazke(2);
+//        biblioteka.zwrocKsiazke(2);
+//        biblioteka.wypozyczKsiazke(2);
+//        biblioteka.zwrocKsiazke(2);
 
         biblioteka.wypozyczKsiazke(3);
         biblioteka.zwrocKsiazke(3);
-        biblioteka.wypozyczKsiazke(3);
-        biblioteka.zwrocKsiazke(3);
+//        biblioteka.wypozyczKsiazke(3);
+//        biblioteka.zwrocKsiazke(3);
 
         biblioteka.wypozyczKsiazke(4);
         biblioteka.zwrocKsiazke(4);
-        biblioteka.wypozyczKsiazke(4);
-        biblioteka.zwrocKsiazke(4);
-        biblioteka.wypozyczKsiazke(4);
-        biblioteka.zwrocKsiazke(4);
-        biblioteka.wypozyczKsiazke(4);
-        biblioteka.zwrocKsiazke(4);
-        biblioteka.wypozyczKsiazke(4);
-        biblioteka.zwrocKsiazke(4);
+//        biblioteka.wypozyczKsiazke(4);
+//        biblioteka.zwrocKsiazke(4);
+//        biblioteka.wypozyczKsiazke(4);
+//        biblioteka.zwrocKsiazke(4);
+//        biblioteka.wypozyczKsiazke(4);
+//        biblioteka.zwrocKsiazke(4);
+//        biblioteka.wypozyczKsiazke(4);
+//        biblioteka.zwrocKsiazke(4);
 
         biblioteka.wypozyczKsiazke(5);
         biblioteka.zwrocKsiazke(5);
