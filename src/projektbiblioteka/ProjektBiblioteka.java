57bfe3a1c9f4f6d8dfec78e4b1904d6a90d89293
Paweł <pawel.bednarz2@gmail.com>
2019-04-15 12:26:50 +0200

    little refactor

--- src/projektbiblioteka/ProjektBiblioteka.java
+++ src/projektbiblioteka/ProjektBiblioteka.java
@@ -5,10 +5,6 @@ public class ProjektBiblioteka {
     public static void main(String[] args) {
 
         Biblioteka biblioteka = new Biblioteka();
-        Seed s = new Seed(biblioteka);
-        s.seed();
-
-
         Menu menu = new Menu(biblioteka);
         menu.wyswietlMenu();
     }
